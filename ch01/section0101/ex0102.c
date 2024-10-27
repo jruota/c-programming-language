d30b49805cd48354246940fa22e4d3273ec9f7cb
Jaroslaw Kolosowski <jarokolosowski@gmail.com>
2024-10-27 16:11:16 +0100

    Section 1.1 - Getting Started

---
+++ ch01/section0101/ex0102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+   printf("hello, world\n");
+   printf("horizontal tab\tHT\n");
+   printf("vertical tab\vVT\n");
+   printf("backspace...\b---BS\n");
+   printf("carriage return\rCR\n");
+   printf("formfeed\tFF\n");
+   printf("audible alert BEL\a\n");
+   return (0);
+}
---
+++ ch01/section0101/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    while (1)
+    {
+        printf("|");
+        printf("\r");
+        printf("/");
+        printf("\r");
+        printf("–");
+        printf("\r");
+        printf("\\");
+        printf("\r");
+    }
+    return (0);
+}
